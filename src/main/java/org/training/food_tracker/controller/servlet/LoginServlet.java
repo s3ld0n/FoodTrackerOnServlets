4ff79ec5ac39f61ac5879201a2382d1fbeefc3a7
Destinat0r <ashavrn@protonmail.com>
2020-01-29 12:51:10 +0200

    singled out credentialsCheck, added redirect, added redirect for GET

--- src/main/java/org/training/food_tracker/controller/servlet/LoginServlet.java
+++ src/main/java/org/training/food_tracker/controller/servlet/LoginServlet.java
@@ -12,7 +12,7 @@ public class LoginServlet extends HttpServlet {
 
     @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        super.doGet(request, response);
+        request.getRequestDispatcher("jsp/login.jsp").forward(request, response);
     }
 
     @Override protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -21,7 +21,18 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
+        validateCredentialsAndSendBackIfNot(request, response, username, password);
+
         System.out.println("username: " + username);
         System.out.println("password: " + password);
+
+        response.sendRedirect(request.getContextPath() + "/students");
+    }
+
+    private void validateCredentialsAndSendBackIfNot(HttpServletRequest request, HttpServletResponse response,
+            String username, String password) throws IOException {
+        if( username == null || username.equals("") || password == null || password.equals("")  ){
+            response.sendRedirect(request.getContextPath() + "/login");
+        }
     }
 }
