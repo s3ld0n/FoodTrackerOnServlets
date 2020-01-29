2e1648f54616ebcaf7699e927ee15247f35c8095
Destinat0r <ashavrn@protonmail.com>
2020-01-29 13:19:42 +0200

    created dao package, added CrudDao

---
+++ src/main/java/org/training/food_tracker/dao/CrudDao.java
@@ -0,0 +1,16 @@
+package org.training.food_tracker.dao;
+
+import java.util.List;
+
+public interface CrudDao<T> {
+    
+    T create(T t);
+    
+    T findById(int id);
+    
+    T update(T t);
+    
+    List<T> findAll();
+    
+    void deleteById(int id);
+}
