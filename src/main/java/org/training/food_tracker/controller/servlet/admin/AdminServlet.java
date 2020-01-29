662ee6a134ded7b761f465284ea92018bd65530c
Destinat0r <ashavrn@protonmail.com>
2020-01-29 13:19:12 +0200

    added AdminServlet

---
+++ src/main/java/org/training/food_tracker/controller/servlet/admin/AdminServlet.java
@@ -0,0 +1,9 @@
+package org.training.food_tracker.controller.servlet.admin;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/admin")
+public class AdminServlet extends HttpServlet {
+
+}
