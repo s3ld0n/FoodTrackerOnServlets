bf1f34126fe623831a3ba265d1e9382245333d3c
Destinat0r <ashavrn@protonmail.com>
2020-01-29 12:38:03 +0200

    created LoginServlet

---
+++ src/main/java/org/training/food_tracker/controller/servlet/LoginServlet.java
@@ -0,0 +1,27 @@
+package org.training.food_tracker.controller.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doGet(request, response);
+    }
+
+    @Override protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        System.out.println("username: " + username);
+        System.out.println("password: " + password);
+    }
+}
