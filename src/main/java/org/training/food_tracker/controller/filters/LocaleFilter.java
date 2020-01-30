dd4dfb51a276e7afd975442adedfca21d7fd4eb0
Destinat0r <ashavrn@protonmail.com>
2020-01-30 14:40:46 +0200

    added logging to filter

--- src/main/java/org/training/food_tracker/controller/filters/LocaleFilter.java
+++ src/main/java/org/training/food_tracker/controller/filters/LocaleFilter.java
@@ -1,6 +1,10 @@
 package org.training.food_tracker.controller.filters;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.training.food_tracker.controller.Servlet;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +16,8 @@ public class LocaleFilter implements Filter {
 
     }
 
+    private static final Logger log = LogManager.getLogger(LocaleFilter.class.getName());
+
     @Override
     public void doFilter(ServletRequest servletRequest,
                          ServletResponse servletResponse,
@@ -19,8 +25,13 @@ public class LocaleFilter implements Filter {
         HttpServletRequest httpReq = (HttpServletRequest) servletRequest;
         HttpServletResponse httpResp = (HttpServletResponse) servletResponse;
 
+        log.debug("inside LocaleFilter");
+
         String localeName = servletRequest.getParameter("lang");
+        log.debug("set locale name: {}", localeName);
+
         if (localeName != null) {
+            log.debug("setting session attribute 'lang' to locale name");
             httpReq.getSession().setAttribute("lang", localeName);
         }
         filterChain.doFilter(httpReq, httpResp);
