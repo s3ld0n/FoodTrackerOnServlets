31393f1917ab3b56649383e1933678b841357371
Destinat0r <ashavrn@protonmail.com>
2020-01-30 18:17:13 +0200

    added DaoException, renamed JDBCDaoFactory -> ConnectionFactory

---
+++ src/main/java/org/training/food_tracker/dao/DaoException.java
@@ -0,0 +1,12 @@
+package org.training.food_tracker.dao;
+
+public class DaoException extends RuntimeException {
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
