fb4bfff84fd638570cd8112b7803a79f098b1e41
Destinat0r <ashavrn@protonmail.com>
2020-01-30 11:09:20 +0200

    commented out @WebServlet

--- src/main/java/org/training/food_tracker/controller/servlet/LoginServlet.java
+++ src/main/java/org/training/food_tracker/controller/servlet/LoginServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/login")
+//@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
     @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
