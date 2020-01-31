1b000ea560c6184557080ea8999034e280390005
Destinat0r <ashavrn@protonmail.com>
2020-01-31 10:40:50 +0200

    added code to execute() method

--- src/main/java/org/training/food_tracker/controller/command/RegistrationCommand.java
+++ src/main/java/org/training/food_tracker/controller/command/RegistrationCommand.java
@@ -1,10 +1,44 @@
 package org.training.food_tracker.controller.command;
 
+import org.training.food_tracker.model.Lifestyle;
+import org.training.food_tracker.model.Role;
+import org.training.food_tracker.model.Sex;
+import org.training.food_tracker.model.User;
+import org.training.food_tracker.service.UserService;
+
 import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 public class RegistrationCommand implements Command {
 
+    private UserService userService = new UserService();
+
     @Override public String execute(HttpServletRequest request) {
-        return null;
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String fullName = request.getParameter("fullName");
+        String nationalName = request.getParameter("nationalName");
+        BigDecimal age = new BigDecimal(request.getParameter("age"));
+        Sex sex = Sex.valueOf(request.getParameter("sex"));
+        BigDecimal weight = new BigDecimal(request.getParameter("weight"));
+        BigDecimal height = new BigDecimal(request.getParameter("height"));
+        Lifestyle lifestyle = Lifestyle.valueOf(request.getParameter("lifestyle"));
+        String password = request.getParameter("password");
+        String passwordConfirm = request.getParameter("passwordConfirm");
+
+        //TODO password check
+
+        userService.create(User.builder()
+                       .username(username)
+                       .email(email)
+                       .fullName(fullName)
+                       .nationalName(nationalName)
+                       .active(false)
+                       .role(Role.USER)
+                       .password(password)
+                       .build());
+
+        return "redirect:/login";
     }
 }
