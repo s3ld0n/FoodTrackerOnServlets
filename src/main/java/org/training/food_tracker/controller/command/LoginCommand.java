6a9a824eb9e39ceaf4f42f441ec76a3ae51ffbb0
Destinat0r <ashavrn@protonmail.com>
2020-01-31 16:00:27 +0200

    added logs

--- src/main/java/org/training/food_tracker/controller/command/LoginCommand.java
+++ src/main/java/org/training/food_tracker/controller/command/LoginCommand.java
@@ -1,5 +1,8 @@
 package org.training.food_tracker.controller.command;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.training.food_tracker.controller.Servlet;
 import org.training.food_tracker.dao.DaoException;
 import org.training.food_tracker.model.Role;
 import org.training.food_tracker.model.User;
@@ -10,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 public class LoginCommand implements Command{
 
     private UserService userService = new UserService();
+    private static final Logger log = LogManager.getLogger(LoginCommand.class.getName());
 
     @Override
     public String execute(HttpServletRequest request) {
@@ -17,6 +21,7 @@ public class LoginCommand implements Command{
         String password = request.getParameter("password");
 
         if( username == null || username.equals("") || password == null || password.equals("")  ){
+            log.debug("Login or password in incorrect");
             return "jsp/login.jsp";
         }
 
