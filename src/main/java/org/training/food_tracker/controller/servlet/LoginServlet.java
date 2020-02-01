5894e912d6353061dd1b210cb42dc6884c404d28
Destinat0r <ashavrn@protonmail.com>
2020-02-01 21:30:08 +0200

    added UserService, added dao, added redirect to user / admin pages

--- src/main/java/org/training/food_tracker/controller/servlet/LoginServlet.java
+++ src/main/java/org/training/food_tracker/controller/servlet/LoginServlet.java
@@ -1,5 +1,10 @@
 package org.training.food_tracker.controller.servlet;
 
+import org.training.food_tracker.dao.DaoException;
+import org.training.food_tracker.model.Role;
+import org.training.food_tracker.model.User;
+import org.training.food_tracker.service.UserService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,8 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@WebServlet("/login")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+
+    private UserService userService;
+
+    @Override public void init() throws ServletException {
+        userService = new UserService();
+    }
 
     @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -23,10 +34,22 @@ public class LoginServlet extends HttpServlet {
 
         validateCredentialsAndSendBackIfNot(request, response, username, password);
 
-        System.out.println("username: " + username);
-        System.out.println("password: " + password);
+        User user;
 
-        response.sendRedirect(request.getContextPath() + "/students");
+        try {
+            user = userService.findByUsername(username);
+        } catch (DaoException e) {
+            e.printStackTrace();
+            request.getRequestDispatcher("jsp/login.jsp").forward(request, response);
+            return;
+        }
+
+        if (user.getRole() == Role.USER) {
+            response.sendRedirect("main.jsp");
+        } else {
+            response.sendRedirect("admin/main");
+        }
+
     }
 
     private void validateCredentialsAndSendBackIfNot(HttpServletRequest request, HttpServletResponse response,
