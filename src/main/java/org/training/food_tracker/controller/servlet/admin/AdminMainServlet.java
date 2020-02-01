94ee46287cfd380da05f448365d224f0f681476e
Destinat0r <ashavrn@protonmail.com>
2020-02-01 21:22:47 +0200

    added AdminServlet, AdminMainServlet

---
+++ src/main/java/org/training/food_tracker/controller/servlet/admin/AdminMainServlet.java
@@ -0,0 +1,17 @@
+package org.training.food_tracker.controller.servlet.admin;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/admin/main")
+public class AdminMainServlet extends HttpServlet {
+
+    @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/jsp/admin/main.jsp").forward(request, response);
+    }
+}
--- src/main/java/org/training/food_tracker/controller/servlet/admin/AdminServlet.java
+++
@@ -1,9 +0,0 @@
-package org.training.food_tracker.controller.servlet.admin;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet("/admin")
-public class AdminServlet extends HttpServlet {
-
-}
