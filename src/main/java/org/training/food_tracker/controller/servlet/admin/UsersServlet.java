711aa25aadec5cbf41fd0aa84c86357fd196de63
Destinat0r <ashavrn@protonmail.com>
2020-02-02 22:39:28 +0200

    added find users

--- src/main/java/org/training/food_tracker/controller/servlet/admin/UsersServlet.java
+++ src/main/java/org/training/food_tracker/controller/servlet/admin/UsersServlet.java
@@ -1,16 +1,40 @@
 package org.training.food_tracker.controller.servlet.admin;
 
+import org.training.food_tracker.dao.DaoException;
+import org.training.food_tracker.model.User;
+import org.training.food_tracker.service.UserService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/admin/users")
 public class UsersServlet extends HttpServlet {
+
+    private UserService userService;
+
+    @Override public void init() throws ServletException {
+        userService = new UserService();
+    }
+
     @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("/jsp/admin/main.jsp").forward(request, response);
+
+        List<User> users;
+
+        try {
+            users = userService.findAll();
+        } catch (DaoException e) {
+            e.printStackTrace(); //TODO add logs
+            request.getRequestDispatcher("/jsp/error.jsp").forward(request, response);
+            return;
+        }
+
+        request.setAttribute("users", users);
+        request.getRequestDispatcher("/jsp/admin/users.jsp").forward(request, response);
     }
 }
