f7cee07ba38174a7e9bed10a4c34149d7113ddc5
Destinat0r <ashavrn@protonmail.com>
2020-02-03 16:37:17 +0200

    fixed bug that led to sql exception - removed statement.executeUpdate()

--- src/main/java/org/training/food_tracker/dao/FoodDao.java
+++ src/main/java/org/training/food_tracker/dao/FoodDao.java
@@ -1,5 +1,7 @@
 package org.training.food_tracker.dao;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.training.food_tracker.dao.impl.ConnectionFactory;
 import org.training.food_tracker.model.Food;
 import org.training.food_tracker.model.User;
@@ -9,7 +11,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class FoodDao implements CrudDao<Food> {
@@ -18,26 +19,31 @@ public class FoodDao implements CrudDao<Food> {
             "SELECT id, name, calories, user_id from food WHERE user_id IS NULL AND name NOT IN "
                                                          + "(SELECT name FROM food WHERE user_id = ?) ORDER BY id DESC";
 
+    private static final Logger log = LogManager.getLogger(FoodDao.class.getName());
+
     @Override public Food create(Food food) throws DaoException {
         return null;
     }
 
     public List<Food> findAllCommonExcludingPersonalByUserId(Long userId) throws DaoException {
         List<Food> foods = new ArrayList<>();
-
+        log.debug("creating connection, making prepared statement");
         try (Connection connection = ConnectionFactory.getConnection();
                 PreparedStatement statement =
                         connection.prepareStatement(FIND_ALL_COMMON_EXCLUDING_PERSONAL_BY_USER_ID_QUERY)) {
             statement.setLong(1, userId);
-            statement.executeUpdate();
 
+            log.debug("executing statement, getting result set and extracting results");
             try (ResultSet resultSet = statement.executeQuery()) {
-                Food food = Food.builder()
-                                    .id(resultSet.getLong("id"))
-                                    .name(resultSet.getString("name"))
-                                    .calories(resultSet.getBigDecimal("calories"))
-                                    .build();
-                foods.add(food);
+                while (resultSet.next()) {
+                    log.debug("creating food");
+                    Food food = Food.builder()
+                                        .id(resultSet.getLong("id"))
+                                        .name(resultSet.getString("name"))
+                                        .calories(resultSet.getBigDecimal("calories"))
+                                        .build();
+                    foods.add(food);
+                }
             }
         } catch (SQLException e) {
             throw new DaoException("selection of common food that does not belong to user has failed", e);
