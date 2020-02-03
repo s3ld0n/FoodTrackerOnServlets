cc8e5e82ae91af23e3e7d50388c5b86504326ae1
Destinat0r <ashavrn@protonmail.com>
2020-02-03 16:42:18 +0200

    added dayServile and currentDay, consumedStatsDTO attributes

--- src/main/java/org/training/food_tracker/controller/servlet/user/UserMainServlet.java
+++ src/main/java/org/training/food_tracker/controller/servlet/user/UserMainServlet.java
@@ -3,10 +3,14 @@ package org.training.food_tracker.controller.servlet.user;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.training.food_tracker.dao.DaoException;
+import org.training.food_tracker.dao.DayDao;
 import org.training.food_tracker.dao.FoodDao;
 import org.training.food_tracker.dto.FoodDTO;
+import org.training.food_tracker.dto.UserDTO;
+import org.training.food_tracker.model.Day;
 import org.training.food_tracker.model.User;
 import org.training.food_tracker.service.ConsumedFoodService;
+import org.training.food_tracker.service.DayService;
 import org.training.food_tracker.service.FoodService;
 import org.training.food_tracker.service.UserService;
 
@@ -24,10 +28,12 @@ public class UserMainServlet extends HttpServlet {
     private FoodService foodService;
 
     private static final Logger log = LogManager.getLogger(UserMainServlet.class.getName());
+    private DayService dayService;
 
     @Override public void init() throws ServletException {
         userService = new UserService();
         foodService = new FoodService(new FoodDao(), new ConsumedFoodService());
+        dayService = new DayService(new DayDao());
     }
 
     @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -39,15 +45,23 @@ public class UserMainServlet extends HttpServlet {
             log.debug("setting allCommonFood");
             request.setAttribute("allCommonFood",
                     foodService.findAllCommonExcludingPersonalByUserIdInDTO(currentUser.getId()));
+
+            log.debug("getting current day");
+            Day currentDay = dayService.getCurrentDayOfUser(currentUser);
+            request.setAttribute("currentDay", currentDay);
+
+            log.debug("getting consumedStatsDTO");
+            request.setAttribute("consumedStatsDTO", dayService.getConsumeStatsForDay(currentDay));
+
         } catch (DaoException e) {
             e.printStackTrace();
         }
 
         request.setAttribute("usersFoodDTOs", foodService.findAllByOwnerInDTOs(currentUser));
 
-
+        UserDTO userDTO = userService.userToUserDTO(currentUser);
         log.debug("setting userDTO {}", userDTO);
-        model.addAttribute("userDTO", userDTO);
+        request.setAttribute("userDTO", userDTO);
 
         request.getRequestDispatcher("/jsp/user/main.jsp").forward(request, response);
     }
