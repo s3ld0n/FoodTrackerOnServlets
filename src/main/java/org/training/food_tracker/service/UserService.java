ea952375e294bebaa753fffe93fada6ad69c846c
Destinat0r <ashavrn@protonmail.com>
2020-02-03 16:43:12 +0200

    added userToUserDTO()

--- src/main/java/org/training/food_tracker/service/UserService.java
+++ src/main/java/org/training/food_tracker/service/UserService.java
@@ -2,6 +2,8 @@ package org.training.food_tracker.service;
 
 import org.training.food_tracker.dao.DaoException;
 import org.training.food_tracker.dao.UserDao;
+import org.training.food_tracker.dto.UserDTO;
+import org.training.food_tracker.model.Biometrics;
 import org.training.food_tracker.model.User;
 
 import java.util.List;
@@ -24,4 +26,24 @@ public class UserService {
     public List<User> findAll() throws DaoException {
         return userDao.findAll();
     }
+
+    public UserDTO userToUserDTO(User user) {
+        Biometrics biometrics = user.getBiometrics();
+        return UserDTO.builder()
+                       .id(user.getId())
+                       .username(user.getUsername())
+                       .fullName(user.getFullName())
+                       .nationalName(user.getNationalName())
+                       .email(user.getEmail())
+                       .age(biometrics.getAge())
+                       .sex(biometrics.getSex())
+                       .weight(biometrics.getWeight())
+                       .height(biometrics.getHeight())
+                       .lifestyle(biometrics.getLifestyle())
+                       .dailyNorm(biometrics.getDailyNorm())
+                       .role(user.getRole())
+                       .password(user.getPassword())
+                       .build();
+    }
+
 }
