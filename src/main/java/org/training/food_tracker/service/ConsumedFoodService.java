03f8cbfd4a261e6a5a15314c70473a0a84573758
Destinat0r <ashavrn@protonmail.com>
2020-02-03 14:00:13 +0200

    added ConsumedFoodService, FoodService

---
+++ src/main/java/org/training/food_tracker/service/ConsumedFoodService.java
@@ -0,0 +1,10 @@
+package org.training.food_tracker.service;
+
+import org.training.food_tracker.dto.FoodDTO;
+
+public class ConsumedFoodService {
+
+    public void registerConsumption(FoodDTO foodDTO) {
+
+    }
+}
