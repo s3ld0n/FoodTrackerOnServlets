f4695c45c8a662601df80e83225c6077ab774630
Destinat0r <ashavrn@protonmail.com>
2020-02-03 15:18:45 +0200

    added FoodService, logger, setAttribute allCommonFood in try-catch, added attributs usersFoodFTOs and userDTO

--- src/main/java/org/training/food_tracker/controller/servlet/user/UserMainServlet.java
+++ src/main/java/org/training/food_tracker/controller/servlet/user/UserMainServlet.java
@@ -1,7 +1,13 @@
 package org.training.food_tracker.controller.servlet.user;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.training.food_tracker.dao.DaoException;
+import org.training.food_tracker.dao.FoodDao;
 import org.training.food_tracker.dto.FoodDTO;
 import org.training.food_tracker.model.User;
+import org.training.food_tracker.service.ConsumedFoodService;
+import org.training.food_tracker.service.FoodService;
 import org.training.food_tracker.service.UserService;
 
 import javax.servlet.ServletException;
@@ -15,9 +21,13 @@ import java.io.IOException;
 public class UserMainServlet extends HttpServlet {
 
     private UserService userService;
+    private FoodService foodService;
+
+    private static final Logger log = LogManager.getLogger(UserMainServlet.class.getName());
 
     @Override public void init() throws ServletException {
         userService = new UserService();
+        foodService = new FoodService(new FoodDao(), new ConsumedFoodService());
     }
 
     @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -25,7 +35,19 @@ public class UserMainServlet extends HttpServlet {
         User currentUser = (User) request.getSession().getAttribute("user");
 
         request.setAttribute("food", new FoodDTO());
+        try {
+            log.debug("setting allCommonFood");
+            request.setAttribute("allCommonFood",
+                    foodService.findAllCommonExcludingPersonalByUserIdInDTO(currentUser.getId()));
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
 
+        request.setAttribute("usersFoodDTOs", foodService.findAllByOwnerInDTOs(currentUser));
+
+
+        log.debug("setting userDTO {}", userDTO);
+        model.addAttribute("userDTO", userDTO);
 
         request.getRequestDispatcher("/jsp/user/main.jsp").forward(request, response);
     }
