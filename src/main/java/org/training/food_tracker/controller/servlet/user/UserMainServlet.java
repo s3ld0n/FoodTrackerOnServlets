4ae727cc5b1b9fd6aa3c22641cf43e3436db4caa
Destinat0r <ashavrn@protonmail.com>
2020-02-03 13:58:06 +0200

    added current user from session and set FoodDTO as an attribute to request

--- src/main/java/org/training/food_tracker/controller/servlet/user/UserMainServlet.java
+++ src/main/java/org/training/food_tracker/controller/servlet/user/UserMainServlet.java
@@ -1,5 +1,7 @@
 package org.training.food_tracker.controller.servlet.user;
 
+import org.training.food_tracker.dto.FoodDTO;
+import org.training.food_tracker.model.User;
 import org.training.food_tracker.service.UserService;
 
 import javax.servlet.ServletException;
@@ -20,6 +22,11 @@ public class UserMainServlet extends HttpServlet {
 
     @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        User currentUser = (User) request.getSession().getAttribute("user");
+
+        request.setAttribute("food", new FoodDTO());
+
+
         request.getRequestDispatcher("/jsp/user/main.jsp").forward(request, response);
     }
 }
