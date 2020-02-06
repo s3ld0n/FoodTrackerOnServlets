908963b36afda17f7fe670c384142f7a3c02a0af
Destinat0r <ashavrn@protonmail.com>
2020-02-06 11:53:34 +0200

    added getDailyNormCalories()

--- src/main/java/org/training/food_tracker/service/UserService.java
+++ src/main/java/org/training/food_tracker/service/UserService.java
@@ -4,8 +4,10 @@ import org.training.food_tracker.dao.DaoException;
 import org.training.food_tracker.dao.UserDao;
 import org.training.food_tracker.dto.UserDTO;
 import org.training.food_tracker.model.Biometrics;
+import org.training.food_tracker.model.Sex;
 import org.training.food_tracker.model.User;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class UserService {
@@ -25,6 +27,27 @@ public class UserService {
 
     public List<User> findAll() throws DaoException {
         return userDao.findAll();
+    }
+
+    /**
+     * Total energy expenditure calculation using Harris–Benedict equation
+     * @return daily norm of calories
+     */
+    public BigDecimal getDailyNormCalories(Biometrics biometrics) {
+        if (biometrics.getSex() == Sex.FEMALE) {
+            return (new BigDecimal(655.1)
+                            .add(new BigDecimal(9.563).multiply(biometrics.getWeight()))
+                            .add(new BigDecimal(1.850).multiply(biometrics.getHeight()))
+                            .subtract(new BigDecimal(4.676).multiply(biometrics.getAge())))
+                           .multiply(biometrics.getLifestyle().getCoefficient())
+                           .setScale(2, BigDecimal.ROUND_HALF_DOWN);
+        }
+        return (new BigDecimal(66.5)
+                        .add(new BigDecimal(13.75).multiply(biometrics.getWeight()))
+                        .add(new BigDecimal(5.003).multiply(biometrics.getHeight()))
+                        .subtract(new BigDecimal(6.755).multiply(biometrics.getAge())))
+                       .multiply(biometrics.getLifestyle().getCoefficient())
+                       .setScale(2, BigDecimal.ROUND_HALF_DOWN);
     }
 
     public UserDTO userToUserDTO(User user) {
