726dc98a6774dd0129aec58673fe305286ed1d0e
Destinat0r <ashavrn@protonmail.com>
2020-02-07 10:22:33 +0200

    added extractConsumedFood()

--- src/main/java/org/training/food/tracker/dao/ConsumedFoodDao.java
+++ src/main/java/org/training/food/tracker/dao/ConsumedFoodDao.java
@@ -2,8 +2,11 @@ package org.training.food.tracker.dao;
 
 import org.training.food.tracker.model.ConsumedFood;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ConsumedFoodDao extends CrudDao<ConsumedFood> {
     List<ConsumedFood> findAllByDayId(Long dayId) throws DaoException;
+    ConsumedFood extractConsumedFood(ResultSet resultSet) throws SQLException;
 }
