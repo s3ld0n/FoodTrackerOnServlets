08de35b67d6065d4e5dd6a4688d9519a857f10df
Destinat0r <ashavrn@protonmail.com>
2020-02-08 20:34:10 +0200

    added consumedFoodDao, constructor

--- src/main/java/org/training/food/tracker/service/defaults/ConsumedFoodServiceDefault.java
+++ src/main/java/org/training/food/tracker/service/defaults/ConsumedFoodServiceDefault.java
@@ -1,11 +1,20 @@
 package org.training.food.tracker.service.defaults;
 
+import org.training.food.tracker.dao.ConsumedFoodDao;
+import org.training.food.tracker.dao.jdbc.ConsumedFoodDaoJDBC;
 import org.training.food.tracker.dto.FoodDTO;
 import org.training.food.tracker.service.ConsumedFoodService;
 
 public class ConsumedFoodServiceDefault implements ConsumedFoodService {
 
+    private ConsumedFoodDao consumedFoodDao;
+
+    public ConsumedFoodServiceDefault() {
+        this.consumedFoodDao = new ConsumedFoodDaoJDBC();
+    }
+
     public void registerConsumption(FoodDTO foodDTO) {
 
     }
+
 }
