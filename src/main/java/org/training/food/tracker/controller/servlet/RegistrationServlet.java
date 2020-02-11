6699de9cc0099a8adb0c206a5d17db23ac90112c
Destinat0r <ashavrn@protonmail.com>
2020-02-11 22:31:11 +0200

    get registration fields from request

--- src/main/java/org/training/food/tracker/controller/servlet/RegistrationServlet.java
+++ src/main/java/org/training/food/tracker/controller/servlet/RegistrationServlet.java
@@ -1,14 +1,23 @@
 package org.training.food.tracker.controller.servlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.training.food.tracker.dto.UserDTO;
+import org.training.food.tracker.model.Lifestyle;
+import org.training.food.tracker.model.Sex;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.math.BigDecimal;
 
 @WebServlet("/registration")
 public class RegistrationServlet extends HttpServlet {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RegistrationServlet.class.getName());
 
     @Override public void init() throws ServletException {
 
@@ -19,8 +28,20 @@ public class RegistrationServlet extends HttpServlet {
         request.getRequestDispatcher("jsp/registration.jsp").forward(request, response);
     }
 
-    @Override protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+    @Override protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        BigDecimal age = new BigDecimal(request.getParameter("age"));
+        Sex sex = Sex.valueOf(request.getParameter("sex"));
+        BigDecimal weight = new BigDecimal(request.getParameter("weight"));
+        BigDecimal height = new BigDecimal(request.getParameter("height"));
+        Lifestyle lifestyle = Lifestyle.valueOf(request.getParameter("lifestyle"));
+        String password = request.getParameter("password");
+        String passwordConfirm = request.getParameter("passwordConfirm");
+
 
     }
 }
