b49708d17e00e779461597234a27b3a30643bdf4
Destinat0r <ashavrn@protonmail.com>
2020-02-12 02:52:14 +0200

    added RegistrationFormValidator, name regex to config, fixed typo in index, commented out RegistrationValidation filter from web.xml

--- src/main/java/org/training/food/tracker/controller/servlet/RegistrationServlet.java
+++ src/main/java/org/training/food/tracker/controller/servlet/RegistrationServlet.java
@@ -2,6 +2,7 @@ package org.training.food.tracker.controller.servlet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.training.food.tracker.controller.validator.RegistrationFormValidator;
 import org.training.food.tracker.dto.UserDTO;
 import org.training.food.tracker.model.Lifestyle;
 import org.training.food.tracker.model.Sex;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 @WebServlet("/registration")
 public class RegistrationServlet extends HttpServlet {
@@ -30,18 +32,22 @@ public class RegistrationServlet extends HttpServlet {
 
     @Override protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         String username = request.getParameter("username");
         String email = request.getParameter("email");
         String firstName = request.getParameter("firstName");
+
+        LOG.debug("validate first name {}", RegistrationFormValidator.validateFirstName(firstName));
+
         String lastName = request.getParameter("lastName");
-        BigDecimal age = new BigDecimal(request.getParameter("age"));
+        BigDecimal age = new BigDecimal(Double.parseDouble(request.getParameter("age")));
         Sex sex = Sex.valueOf(request.getParameter("sex"));
-        BigDecimal weight = new BigDecimal(request.getParameter("weight"));
-        BigDecimal height = new BigDecimal(request.getParameter("height"));
+        BigDecimal weight = new BigDecimal(Double.parseDouble(request.getParameter("weight")));
+        BigDecimal height = new BigDecimal(Double.parseDouble(request.getParameter("height")));
         Lifestyle lifestyle = Lifestyle.valueOf(request.getParameter("lifestyle"));
         String password = request.getParameter("password");
         String passwordConfirm = request.getParameter("passwordConfirm");
 
-
+        LOG.debug("obtained attributes : {}, {}, {}, {}, {}", username, email, firstName, lastName, age);
     }
 }
--- src/main/webapp/jsp/index.jsp
+++ src/main/webapp/jsp/index.jsp
@@ -14,7 +14,7 @@
     <meta http-equiv="Content-Type" content="text/html" charset="utf-8" >
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel='stylesheet' type="text/css" href="webjars/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstra pcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <style>
         <%@ include file="/static/css/style.css" %>
