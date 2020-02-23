7a0466a19c40d887bde853096c8e9db48a17a679
Destinat0r <ashavrn@protonmail.com>
2020-02-23 20:21:32 +0200

    errors/ moved to root of webapp

--- src/main/java/org/training/food/tracker/controller/servlet/admin/AdminFoodListDeleteFoodServlet.java
+++ src/main/java/org/training/food/tracker/controller/servlet/admin/AdminFoodListDeleteFoodServlet.java
@@ -31,7 +31,7 @@ public class AdminFoodListDeleteFoodServlet extends HttpServlet {
             foodService.deleteCommonFoodByName(request.getParameter("name"));
         } catch (DaoException e) {
             LOG.error("Deletion failed - error occurred.");
-            request.getRequestDispatcher("/WEB-INF/jsp/errors/500.jsp").forward(request, response);
+            request.getRequestDispatcher("/errors/500.jsp").forward(request, response);
             return;
         }
 
--- src/main/java/org/training/food/tracker/controller/servlet/user/UserDeleteFoodServlet.java
+++ src/main/java/org/training/food/tracker/controller/servlet/user/UserDeleteFoodServlet.java
@@ -46,7 +46,7 @@ public class UserDeleteFoodServlet extends HttpServlet {
             foodService.deleteByNameAndUserId(foodName, user);
         } catch (DaoException e) {
             LOG.error("doPost() :: food deletion failed");
-            request.getRequestDispatcher("/WEB-INF/jsp/errors/500.jsp").forward(request, response);
+            request.getRequestDispatcher("/errors/500.jsp").forward(request, response);
             return;
         }
 
